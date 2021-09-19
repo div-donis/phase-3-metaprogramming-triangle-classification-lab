6ff2dac7deb04fe7b5bfb34a5b16ddd785216d55
Don Odom <divdonis@gmail.com>
2021-09-19 15:04:47 -0700

    commit tree 32130674dda77f50adc4dfd5cd8c7cb71bc71a61

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+  def kind
+    if x > 0 && y > 0 && z > 00 && (x + y > z  || x + z > y || y + z > x)
+      if x == y && y == z
+      :equilateral
+      elsif x == y && y != z || x == z && z !=y || y == z && z != x
+        :isosceles
+      else
+        :scalene
+      end  
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "error"
+    end
+  end
 end
